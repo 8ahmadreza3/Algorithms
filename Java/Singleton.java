5d631060280ab9de8b71d18f0cf0a6b7c4cddf47
8ahmadreza3 <8arhp3@gmail.com>
2024-03-24 12:18:40 +0330

    update readme

--- Java/Singleton.java
+++ Java/Singleton.java
@@ -1,17 +1,18 @@
 public class Singleton {
 
-    //private static variable of the same class that has a private access modifier
-    private static Singleton single_instance = null;
+  //private static variable of the same class that has a private access modifier
+  private static Singleton single_instance = null;
 
-    // private constructor restricted to this class itself
-    private Singleton() {
-        // prevent the instantiation of this class
-    }
+  // private constructor restricted to this class itself
+  private Singleton() {
+    // prevent the instantiation of this class
+  }
 
-    // static method to create instance of Singleton class
-    public static Singleton getInstance() {
-        if (single_instance == null)
-            single_instance = new Singleton();
-        return single_instance;
+  // static method to create instance of Singleton class
+  public static Singleton getInstance() {
+    if (single_instance == null){
+      single_instance = new Singleton();
     }
+    return single_instance;
+  }
 }
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 **Welcome to this repository. Here I am trying to put the algorithms and applied methods that I saw in different educational courses.**
 
 **Programming languages:**
-- ![C](https://img.shields.io/badge/c-%2300599C.svg?style=for-the-badge&logo=c&logoColor=white)
+- ![C](https://img.shields.io/badge/c-%2300599C.svg?style=for-the-badge&logo=c&logoColor=white) 
+- ![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white)
