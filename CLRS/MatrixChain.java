d8cb40d6ae7c7679b343ec1587e37a66081fe006
8ahmadreza3 <8arhp3@gmail.com>
2024-03-29 19:41:33 +0330

    matrixchain bottom up

--- CLRS/MatrixChain.java
+++ CLRS/MatrixChain.java
@@ -1,18 +1,40 @@
 class MatrixChain{
 
-  static int matrixChainBU(int[] p, int n){
-    int[][] r = new int[n][n];
+  static int matrixChainBU(int[] p, int n, int[][] s){
+    int[][] r = new int[n][n] ;
+    for (int i = 1; i < n; i++)
+      r[i][i] = 0;
+    for(int l=2 ; l<n ; ++l){
+      for(int i=1 ; i < n-l+1 ; i++){
+        int j = i + l - 1 ;
+        if (j == n)
+          continue;
+        int min = Integer.MAX_VALUE ;
+        for(int k=i ; k<j ; ++k){
+          int q = r[i][k] + r[k+1][j] + p[i-1] * p[k] * p[j] ;
+          if(q < min){
+            min = q ;
+            s[i][j] = k ;
+          }
+        }
+        r[i][j] = min ;
+      }
+    }
+    return r[1][n-1] ;
   }
 
-  static int matrixChainTD(int[] p, int i, int j, int[][] r){
-    if(j <= i)
+  static int matrixChainTD(int[] p, int i, int j, int[][] r, int[][] s){
+    if(j <= i){
       return 0;
-    if(r[i][j] > 0)
+    }
+    if(r[i][j] > 0){
       return r[i][j] ;
+    }
     int min = Integer.MAX_VALUE ;
     for(int k=i ; k<j ; ++k){
-      int q = matrixChainTD(p, i, k, r) + matrixChainTD(p, k+1, j, r) + p[i] * p[k+1] * p[j+1] ;
+      int q = matrixChainTD(p, i, k, r, s) + matrixChainTD(p, k+1, j, r, s) + p[i-1] * p[k] * p[j] ;
       if(q < min){
+        s[i][j] = k ;
         min = q ;
       }
     }
@@ -21,7 +43,11 @@ class MatrixChain{
   }
 
   public static void main(String[] args){
-
-
+    int[] p = {4, 5, 11, 17, 6, 7} ;
+    int size = p.length ;
+    int[][] r = new int[size][size] ;
+    int[][] s = new int[size][size] ;
+    System.out.println("Top Down: Minimum Cost is " + matrixChainTD(p, 1 , size-1, r, s)) ;
+    System.out.println("Bottom Up: Minimum Cost is " + matrixChainBU(p, size, s)) ;
   }
 }
