1b73f9ffef16f3f8dcfddcbf991e72fa9412a4f5
8ahmadreza3 <8arhp3@gmail.com>
2024-03-29 18:31:07 +0330

    print rodcut

--- CLRS/Rodcut.java
+++ CLRS/Rodcut.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
 
 class Rodcut {
-  static int rodcutBU(int[] price, int n){
+
+  static int rodcutBU(int[] price, int n, int[] s){
     int[] r = new int[n+1];
     r[0] = 0 ;
     r[1] = price[1] ;
@@ -11,6 +12,7 @@ class Rodcut {
         int p = price[k] + r[i-k];
         if(p > max){
           max = p ;
+          s[i] = k ;
         }
       }
       r[i] = max ;
@@ -18,7 +20,7 @@ class Rodcut {
     return r[n] ;
   }
 
-  static int rodcutTD(int[] price, int index, int[] r){
+  static int rodcutTD(int[] price, int index, int[] r, int[]s){
     if (index <= 1) {
       return price[index];
     }
@@ -27,20 +29,33 @@ class Rodcut {
     }
     int max = Integer.MIN_VALUE;
     for(int k=1 ; k<=index ; ++k){
-      int p = price[k] + rodcutTD(price, index - k, r);
+      int p = price[k] + rodcutTD(price, index - k, r, s);
       if(p > max){
         max = p ;
+        s[index] = k ;
       }
     }
     r[index] = max ;
     return max ;
     }
 
+    static void printRodcut(int[] s, int n){
+      if(n <= 1){
+        System.out.println("No Cut");
+        return ;
+      }
+      System.out.println("Cut:" + s[n]);
+      printRodcut(s, n-s[n]);
+    }
+
   public static void main(String[] args){
-    int[] price = { 0, 5, 7};
+    int[] price = { 0, 5, 11 };
     int size = price.length;
     int[] r = new int[size];
-    System.out.println("Top Down: Maximum Value is " + rodcutTD(price, size - 1, r));
-    System.out.println("Bottom Up: Maximum Value is " + rodcutBU(price, size - 1));
+    int[] s = new int[size];
+    System.out.println("Top Down: Maximum Value is " + rodcutTD(price, size - 1, r, s));
+    printRodcut(s, size-1);
+    System.out.println("Bottom Up: Maximum Value is " + rodcutBU(price, size - 1, s));
+    printRodcut(s, size-1);
   }
 }
