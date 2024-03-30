1eafe1d1dd7edd87a3c1d8d802372506779caee7
8ahmadreza3 <8arhp3@gmail.com>
2024-03-30 13:09:09 +0330

    change making bottom up

--- CLRS/ChangeMaking.java
+++ CLRS/ChangeMaking.java
@@ -1,4 +1,23 @@
 class ChangeMaking {
+
+  static int changeMakingBU(int[] coins, int n, int m) {
+    int[][] r = new int[n + 1][m + 1] ;
+    for (int j=1 ; j<=m ; ++j) {
+      r[0][j] = Integer.MAX_VALUE ;
+      for (int i=1 ; i<=n ; ++i) {
+        r[i][0] = 0 ;
+        if (coins[i] > j) {
+          r[i][j] = r[i - 1][j] ;
+        } else {
+          int q = r[i-1][j] ;
+          int p = r[i][j-coins[i]] + 1 ;
+          r[i][j] = Math.min(q, p);
+        }
+      }
+    }
+    return r[n][m] ;
+  }
+
   static int changeMakingTD(int[] coins, int n, int m, int[][] r) {
     if (m == 0) {
       return 0 ;
@@ -20,7 +39,7 @@ class ChangeMaking {
         return p ;
       } else {
         r[n][m] = q ;
-          return q ;
+        return q ;
       }
     }
   }
@@ -36,6 +55,7 @@ class ChangeMaking {
       }
     }
 
-    System.out.println("Top Down: Minimum number of coins is: " + changeMakingTD(coins,n, amount, r));
+    System.out.println("Bottom Up: Minimum number of coins is: " + changeMakingBU(coins, n, amount));
+    System.out.println("Top Down: Minimum number of coins is: " + changeMakingTD(coins, n, amount, r));
   }
 }
