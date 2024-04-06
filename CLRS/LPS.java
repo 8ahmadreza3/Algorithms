9aba1217776efd2dbf56a69aae0121c1a40da88a
8ahmadreza3 <8arhp3@gmail.com>
2024-04-06 16:33:50 +0330

    lps Top Down

--- CLRS/LPS.java
+++ CLRS/LPS.java
@@ -20,8 +20,34 @@ public class LPS {
     return c[0][n-1] ;
   }
 
+  public static int lpsTD(char[] x, int i, int j, int[][] r) {
+    if ((i == 0 || j == 0) && (i > j)) {
+      return 0 ;
+    }
+    if (i == j) {
+      return 1 ;
+    }
+    if (r[i][j] > 0) {
+      return r[i][j] ;
+    }
+    if (x[i] == x[j]) {
+      int p = lpsTD(x, i+1, j-1, r) + 2 ;
+      r[i][j] = p ;
+      return p ;
+    } else {
+      int p = lpsTD(x, i, j-1, r) ;
+      int q = lpsTD(x, i+1, j, r) ;
+      int max = Math.max(p, q) ;
+      r[i][j] = max ;
+      return r[i][j] ;
+    }
+  }
+
   public static void main(String[] args) {
     char[] x = "ahamdreza".toCharArray() ;
-    System.out.println("Bottom Up: Longest palindromic substring is: " + lpsBU(x));
+    int length = x.length ;
+    int[][] r = new int[length][length] ;
+    System.out.println("Bottom Up: Longest palindromic substring is: " + lpsBU(x)) ;
+    System.out.println("Top Down: Longest palindromic substring is: " + lpsTD(x, 0, length-1, r)) ;
   }
 }
