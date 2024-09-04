5fd49541de1fa48d5e54f29fd4dec479647cf6eb
8ahmadreza3 <8arhp3@gmail.com>
2024-09-04 15:41:30 +0330

    mst kruskals

--- src/MST.java
+++ src/MST.java
@@ -50,3 +50,73 @@ class MST {
         printMST(parent, graph);
     }
 }
+
+class KruskalsMST {
+    static class Edge {
+        int src, dest, weight;
+        public Edge(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+    }
+
+    static class Subset {
+        int parent, rank;
+        public Subset(int parent, int rank) {
+            this.parent = parent;
+            this.rank = rank;
+        }
+    }
+
+    private static void kruskals(int V, List<Edge> edges) {
+        int j = 0;
+        int noOfEdges = 0;
+        Subset subsets[] = new Subset[V];
+        Edge results[] = new Edge[V];
+
+        for (int i = 0; i < V; i++)
+            subsets[i] = new Subset(i, 0);
+
+        while (noOfEdges < V - 1) {
+            Edge nextEdge = edges.get(j);
+            int x = findRoot(subsets, nextEdge.src);
+            int y = findRoot(subsets, nextEdge.dest);
+            if (x != y) {
+                results[noOfEdges] = nextEdge;
+                union(subsets, x, y);
+                noOfEdges++;
+            }
+            j++;
+        }
+
+        int minCost = 0;
+        for (int i = 0; i < noOfEdges; i++) {
+            System.out.println(results[i].src + " _ " + results[i].dest + " == " + results[i].weight);
+            minCost += results[i].weight;
+        }
+        System.out.println("Total cost of MST: " + minCost);
+    }
+
+    private static void union(Subset[] subsets, int x, int y) {
+        int rootX = findRoot(subsets, x);
+        int rootY = findRoot(subsets, y);
+        if (subsets[rootY].rank < subsets[rootX].rank)
+            subsets[rootY].parent = rootX;
+        else if (subsets[rootX].rank < subsets[rootY].rank)
+            subsets[rootX].parent = rootY;
+        else {
+            subsets[rootY].parent = rootX;
+            subsets[rootX].rank++;
+        }
+    }
+
+    private static int findRoot(Subset[] subsets, int i) {
+        if (subsets[i].parent == i)
+            return subsets[i].parent;
+
+        subsets[i].parent
+                = findRoot(subsets, subsets[i].parent);
+        return subsets[i].parent;
+    }
+}
